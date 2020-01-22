e1a6e23e9532080dba596ef6b60034784710064c
ThomasPostMeridian <thomasmcroberts@utexas.edu>
2020-01-22 11:00:34 -0600

    Finished word compare function

--- main.c
+++ main.c
@@ -6,21 +6,20 @@
 
 
 
-int in_dict(const char* word, const char* dict, size_t dict_size){
+// Create the dictionary string
+void create_dict(FILE *fp, char *dictionary);
+// Check if it's in the dictionary
+int in_dict(const char* word, const char* dict, size_t dict_size);
 
 
-}
 
 
 
 int main(void){
 	FILE *fp;	
-	FILE *fpout;	
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
 
 
+// Read dictionary file
 	if ((fp = fopen("dict.txt","r")) == NULL){
 	   printf("Error opening file");
 	   // Program exits if the file pointer returns NULL.
@@ -34,23 +33,81 @@ int main(void){
     char *dict = (char *) malloc(sz*sizeof(char));
     dict[0] = '\0';
 
-	while ((read = getline(&line, &len, fp)) != -1) {
-        line[strlen(line)-1] = ' '; 
-        line[strlen(line)] = '\0'; 
-        strcat(dict, line); 
-    }
 
+    create_dict(fp, dict);
+
+
+/* Check dictionary's creation
     char *buffer = (char *) malloc(50*sizeof(char));
-    memcpy(buffer, dict, 50); 
-
+    memcpy(buffer, &dict[20000], 50); 
     printf("%s\n", buffer);
+*/
 
 	fclose(fp);
-	if(line){
-		free(line);
-    }
     free(dict);
-    free(buffer);
 
 	exit(0);
 }
+
+
+
+// Create the dictionary string
+void create_dict(FILE *fp, char *dictionary){
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    // Note: for the sake of simplicity and speed this function makes assumptions about 
+    // the input dictionary file
+	while ((read = getline(&line, &len, fp)) != -1) {
+        if(srlen(line) > 1){
+            for(int i = 0; i < strlen(line); i++){
+                line[i] = tolower(line[i]);
+            }
+            line[strlen(line)-1] = ' '; 
+            line[strlen(line)] = '\0'; 
+            strcat(dictionary, line); 
+        }
+    }
+    
+	if(line){
+		free(line);
+    }
+
+}
+
+// -: a < b, 0: a = b, +: a > b
+int word_compare(const char* a, const char* b){
+    int comp = 0;
+    int i = 0;
+    for(i = 0; i < strlen(a); i++){
+        if(b[i] = '\0'){
+            // a is longer than b
+            return 1;
+        }
+
+        comp = b[i] - a[i];    
+        if(comp != 0){
+            return comp;
+        }
+    }
+    if(b[i] = '\0'){
+        // a and b are the same
+        return 0;
+    }
+
+    // b is longer than a
+    return -1;
+
+}
+
+// Check if it's in the dictionary
+int in_dict(const char* word, const char* dict, size_t dict_size){
+    for(int i = 0; i < dict_size; i++){
+    }
+        
+
+}
+
+
+
